30cd047285becaf414aac0c18ae5aab3f359ba53
Oyiyi <yihan7206@gmail.com>
2023-08-21 23:26:36 -0700

    draft mimic vector database by csv, converting to embeddings, building the search function

--- ebd/ebd.py
+++ ebd/ebd.py
@@ -5,23 +5,11 @@ from sentence_transformers import SentenceTransformer
 # settings: define which model to use. Size 384 for each sentence.
 ebd_model = SentenceTransformer('all-MiniLM-L6-v2') 
 
-# todo to bring true inputs
-#Our sentences we like to encode
-sentences = ['This framework generates embeddings for each input sentence',
-    'Sentences are passed as a list of string.',
-    'The quick brown fox jumps over the lazy dog.']
-
-#Sentences are encoded by calling model.encode()
-ebds = ebd_model.encode(sentences)
-
-#Print the embeddings
-for sentence, ebd in zip(sentences, ebds): # zip combines nth and nth
-    print("Sentence:", sentence)
-    print("Embedding:", ebd)
-    print("")
-
-# ebd into dataframe (n rows for each sentences x 384 each) (3 rows x 384 columns)
-ebds = pd.DataFrame(ebds)
-print(ebds)
-
-# todo how to connect multi index 1. searched index from semantic search; 2. gallery-exhibit manual index
+# convert data in database into embeddings, save to output.csv
+db_texts_csv_path = 'db/exhibit-info.csv'
+db_ebds_csv_path = 'db/exhibit-info-ebds.csv' # path to save embedded texts from db
+db_texts = pd.read_csv(db_texts_csv_path, header=None, squeeze=True, encoding='iso-8859-1')
+db_ebds = [ebd_model.encode(i) for i in db_texts] # convert_to_tensor=True pytorch tensor containing embeddings
+db_ebds_df = pd.DataFrame(db_ebds)
+db_ebds_df.to_csv(db_ebds_csv_path, index=False, header=False)
+print("......texts in database embedded and saved successfully.")
---
+++ search/search_client.py
@@ -0,0 +1,23 @@
+﻿from sentence_transformers import SentenceTransformer, util
+import torch
+import pandas as pd
+
+# user input -> input converts into ebds
+user_input = input("User: ")
+ebd_model = SentenceTransformer('all-MiniLM-L6-v2') 
+user_input_ebds = [ebd_model.encode(user_input)] # convert_to_tensor=True pytorch tensor containing embeddings
+user_input_ebds_df = torch.FloatTensor(pd.DataFrame(user_input_ebds))
+
+# read db embeddings as df
+db_ebds_csv_path = 'db/exhibit-info-ebds.csv'
+db_ebds_df = torch.FloatTensor(pd.read_csv(db_ebds_csv_path))
+
+
+# ebds asymmetric semantic search against database, model: msmarco-distilroberta-base-v3
+hits = util.semantic_search(user_input_ebds_df, db_ebds_df, top_k=1)
+# todo question: doable to compare two DataFrame? What should be the format considering efficiency?
+print(hits)
+
+# pull database's information as text
+# text + user inputs as inputs of calling LLM APIs
+
