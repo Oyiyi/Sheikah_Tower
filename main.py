5fd2bcc1a05b1a5b5fee0905104b16de60192059
Yi <63529538+Oyiyi@users.noreply.github.com>
2023-08-31 23:45:16 -0700

    updated main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-﻿# the functiond to achieve in this script:
-#   a sample embeddings dataset in .csv that was indexed
-#   mechanism that converted the new input into embeddings quickly
-#   sentence-transformer semantic search new dataset vs. database (.cvs dataset)
-#   index to pull the needed data from database to use as an input in LLM
-#   give LLM the neccesary input + user live instruction, to generate best results
+﻿""" 
+Steps:
+  a sample embeddings dataset in .csv that was indexed
+  mechanism that converted the new input into embeddings quickly
+  sentence-transformer semantic search new dataset vs. database (.cvs dataset)
+  index to pull the needed data from database to use as an input in LLM
+  give LLM the neccesary input + user live instruction, to generate best results
+"""
 
 import openai
 from environment import OPENAI_API_KEY
